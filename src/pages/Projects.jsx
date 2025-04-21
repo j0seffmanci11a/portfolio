716fe62de0dda467d802cb0145bb881d953a8619
j0seffmanci11a <0903jmm@gmail.com>
2025-04-21 13:18:54 -0400

    5th commit - Images

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,21 +6,21 @@ export default function Projects() {
 
       <div className="project">
         <h3>Todo App</h3>
-        <img src="/todo.png" alt="Todo App" style={{width: '500px', height:'auto'}} />
+        <img src={process.env.PUBLIC_URL + "/todo.png"} alt="Todo App" style={{width: '500px', height:'auto'}} />
         <p>A basic task management app built using React Native and Expo, site link in the comments</p>
         <a href=" https://youtu.be/rm4-zfmUhFg" target="_blank">View on Youtube.</a>
       </div>
 
       <div className="project">
         <h3>Exercise App</h3>
-        <img src="/exercise.png" alt="Exercise App" style={{width: '500px', height:'auto'}}/>
+        <img src={process.env.PUBLIC_URL + "/exercise.png"} alt="Exercise App" style={{width: '500px', height:'auto'}}/>
         <p>A basic exercise tracker app, site link in comments</p>
         <a href=" https://youtu.be/2ceuuEmhejI" target="_blank">View on Youtube.</a>
       </div>
 
       <div className="project">
         <h3>Quiz App</h3>
-        <img src="/quiz.png" alt="Quiz App" style={{width: '500px', height:'auto'}}/>
+        <img src={process.env.PUBLIC_URL + "/quiz.png"} alt="Quiz App" style={{width: '500px', height:'auto'}}/>
         <p>A basic quiz app, site link in the comments.</p>
         <a href=" https://youtu.be/EX1hHGo-Oe8" target="_blank">View on Youtube.</a>
       </div>
